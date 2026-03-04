a071161231400780579c86dbe367a3ac445741db
huangjinzhao <huangjinzhao@jd.com>
2026-03-04 18:37:02 +0800

    fix

---
+++ src/main/java/com/ysu/edu/User.java
@@ -0,0 +1,40 @@
+package com.ysu.edu;
+
+public class User {
+    private Long id;
+    private String name;
+    private int score;
+    private int level;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
